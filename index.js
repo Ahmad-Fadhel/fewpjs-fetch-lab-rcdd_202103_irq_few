f2b6ec78748fb9fc62654311aa792490fd71d6b8
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-09 18:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  fetch('https://anapioficeandfire.com/api/books')
   .then(function (responce){
     return responce.JSON();
   })
